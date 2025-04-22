146c4ad2834688ac2af58f282e3329a58222d46c
Giang Nguyen <64316740+HoangGiang93@users.noreply.github.com>
2025-04-22 21:25:46 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,10 +5,22 @@ start_time=$(date +%s)
 CURRENT_DIR=$PWD
 cd $(dirname $0)
 SRC_DIR=$PWD/src
+
+EXT_DIR=$PWD/ext
+CMAKE_DIR=$EXT_DIR/CMake
+if [ ! -d "$CMAKE_DIR" ]; then
+    mkdir -p $CMAKE_DIR
+    CMAKE_TAR_FILE=cmake-4.0.0-linux-x86_64.tar.gz
+    curl -L -o $EXT_DIR/$CMAKE_TAR_FILE https://github.com/Kitware/CMake/releases/download/v4.0.0/$CMAKE_TAR_FILE
+    tar xf $EXT_DIR/$CMAKE_TAR_FILE -C $CMAKE_DIR --strip-components=1
+    rm -f $EXT_DIR/$CMAKE_TAR_FILE
+fi
+CMAKE_EXECUTABLE=$CMAKE_DIR/bin/cmake
+
 MUJOCO_VERSION=3.3.0
 MUJOCO_SRC_DIR=$SRC_DIR/mujoco-$MUJOCO_VERSION
-if [ ! -d $SRC_DIR ]; then
-    mkdir -p $SRC_DIR
+if [ ! -d $MUJOCO_SRC_DIR ]; then
+    mkdir -p $MUJOCO_SRC_DIR
     MUJOCO_TAR_FILE=mujoco-$MUJOCO_VERSION.tar.gz
     curl -L -o $SRC_DIR/$MUJOCO_TAR_FILE https://github.com/google-deepmind/mujoco/archive/refs/tags/$MUJOCO_VERSION.tar.gz
     tar xf $SRC_DIR/$MUJOCO_TAR_FILE -C $MUJOCO_SRC_DIR --strip-components=1
@@ -31,9 +43,9 @@ if ! grep -Fxq "$LINE_TO_ADD" "$CMAKE_PATH"; then
     echo "$LINE_TO_ADD" >> $CMAKE_PATH
 fi
 
-cmake -S $MUJOCO_SRC_DIR -B $BUILD_DIR -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DMUJOCO_BUILD_EXAMPLES=OFF -DMUJOCO_BUILD_TESTS=OFF -DMUJOCO_BUILD_SIMULATE=ON -DMUJOCO_TEST_PYTHON_UTIL=OFF
-cmake --build $BUILD_DIR
-cmake --install $BUILD_DIR
+$CMAKE_EXECUTABLE -S $MUJOCO_SRC_DIR -B $BUILD_DIR -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DMUJOCO_BUILD_EXAMPLES=OFF -DMUJOCO_BUILD_TESTS=OFF -DMUJOCO_BUILD_SIMULATE=ON -DMUJOCO_TEST_PYTHON_UTIL=OFF -DCMAKE_POLICY_VERSION_MINIMUM="3.5" -Wno-deprecated -Wno-dev
+$CMAKE_EXECUTABLE --build $BUILD_DIR
+$CMAKE_EXECUTABLE --install $BUILD_DIR
 
 MUJOCO_PLUGIN_DIR=$PWD/mujoco_plugin/mujoco-$MUJOCO_VERSION
 if [ ! -d $MUJOCO_PLUGIN_DIR ]; then
