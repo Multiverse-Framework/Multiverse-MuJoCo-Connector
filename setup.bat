86582a7fb23e92c5fedaed7c16e4dbd4de86128b
Giang Nguyen <hoanggia@uni-bremen.de>
2025-04-22 18:52:24 +0200

    Add build for Windows

--- setup.bat
+++ setup.bat
@@ -6,25 +6,34 @@ for /f %%a in ('powershell -Command "[int](Get-Date -UFormat %%s)"') do set STAR
 set CURRENT_DIR=%CD%
 cd /d %~dp0
 
-set "MSYS2_DIR=%CD%\ext\msys2"
-set "MINGW32_CMAKE_EXE=%MSYS2_DIR%\mingw64\bin\cmake.exe"
+set "EXT_DIR=%CD%\ext"
+set "CMAKE_DIR=%EXT_DIR%\cmake"
+set "CMAKE_FILE_NAME=cmake-4.0.1-windows-x86_64"
+set "CMAKE_ZIP_FILE=%CMAKE_FILE_NAME%.zip"
+if not exist "%CMAKE_DIR%" (
+    if not exist "%EXT_DIR%\%CMAKE_ZIP_FILE%" (
+        powershell -NoProfile -Command "curl -o '%EXT_DIR%\%CMAKE_ZIP_FILE%' 'https://github.com/Kitware/CMake/releases/download/v4.0.1/%CMAKE_ZIP_FILE%'"
+    )
+    powershell -NoProfile -Command "7z x '%EXT_DIR%\%CMAKE_ZIP_FILE%' -o'%EXT_DIR%'"
+    powershell -NoProfile -Command "move '%EXT_DIR%\%CMAKE_FILE_NAME%' '%CMAKE_DIR%'"
+    del "%EXT_DIR%\%CMAKE_ZIP_FILE%"
+)
+set "CMAKE_EXECUTABLE=%CMAKE_DIR%\bin\cmake.exe"
 
-if not exist "%MSYS2_DIR%" (
-    mkdir "%MSYS2_DIR%"
-    powershell -NoProfile -Command "curl -o '%MSYS2_DIR%\msys2-x86_64-20241208.exe' 'https://github.com/msys2/msys2-installer/releases/download/2025-02-21/msys2-x86_64-20250221.exe'; %MSYS2_DIR%\msys2-x86_64-20241208.exe in --confirm-command --accept-messages --root %MSYS2_DIR%"
-    powershell -NoProfile -Command "%MSYS2_DIR%\msys2_shell.cmd -defterm -here -no-start -c 'pacman -Syu --noconfirm'"
-    powershell -NoProfile -Command "%MSYS2_DIR%\msys2_shell.cmd -defterm -here -no-start -c 'pacman -Sy --noconfirm mingw-w64-x86_64-gcc mingw-w64-x86_64-make mingw-w64-x86_64-cmake'"
-    powershell -NoProfile -Command "%MSYS2_DIR%\msys2_shell.cmd -defterm -here -no-start -c 'pacman -Syu --noconfirm'"
-    echo MSYS2 installation complete.
-) else (
-    echo MSYS2 already installed. Updating packages...
-    powershell -NoProfile -Command "%MSYS2_DIR%\msys2_shell.cmd -defterm -here -no-start -c 'pacman -Syu --noconfirm'"
-    echo Updating packages complete.
+set "CMAKE_BUILD_DIR=%EXT_DIR%\vcpkg"
+set "VCPKG_DIR=%EXT_DIR%\vcpkg"
+if not exist "%VCPKG_DIR%" (
+    git clone https://github.com/Microsoft/vcpkg.git "%VCPKG_DIR%"
+    cd "%VCPKG_DIR%"
+    bootstrap-vcpkg.bat
+    cd %EXT_DIR%
+    .\vcpkg\vcpkg.exe install
 )
 
 set SRC_DIR=%CD%\src
 
 set MUJOCO_VERSION=3.3.0
+set MUJOCO_SRC_DIR=%SRC_DIR%\mujoco-%MUJOCO_VERSION%
 set MUJOCO_ZIP_FILE=mujoco-%MUJOCO_VERSION%.zip
 if not exist "%SRC_DIR%" (
     mkdir "%SRC_DIR%"
@@ -32,7 +41,7 @@ if not exist "%SRC_DIR%" (
     powershell -NoProfile -Command "7z x '%SRC_DIR%\%MUJOCO_ZIP_FILE%' -o'%SRC_DIR%'"
     del "%SRC_DIR%\%MUJOCO_ZIP_FILE%"
 )
-set MUJOCO_SRC_DIR=%SRC_DIR%\mujoco-%MUJOCO_VERSION%
+
 set BUILD_DIR=%CD%\build\mujoco-%MUJOCO_VERSION%
 set INSTALL_DIR=%CD%\install\mujoco-%MUJOCO_VERSION%
 if not exist "%INSTALL_DIR%" (
@@ -42,20 +51,20 @@ if not exist "%BUILD_DIR%" (
     mkdir "%BUILD_DIR%"
 )
 
-xcopy /E /I /Y "%CD%\plugin\multiverse_connector" "%MUJOCO_SRC_DIR%\mujoco-%MUJOCO_VERSION%\plugin\multiverse_connector"
+xcopy /E /I /Y "%CD%\plugin\multiverse_connector" "%MUJOCO_SRC_DIR%\plugin\multiverse_connector"
 set CMAKE_PATH=%MUJOCO_SRC_DIR%\CMakeLists.txt
-@REM set LINE_TO_ADD=add_subdirectory(plugin/multiverse_connector^^^)
-@REM findstr /C:"%LINE_TO_ADD%" "%CMAKE_PATH%" >nul
-@REM if errorlevel 1 (
-@REM     echo %LINE_TO_ADD%>> "%CMAKE_PATH%"
-@REM )
+set LINE_TO_ADD=add_subdirectory(plugin/multiverse_connector^^^)
+findstr /C:"plugin/multiverse_connector" "%CMAKE_PATH%" >nul
+if errorlevel 1 (
+    echo %LINE_TO_ADD%>> "%CMAKE_PATH%"
+)
 
-%MINGW32_CMAKE_EXE% -S "%MUJOCO_SRC_DIR%" -B "%BUILD_DIR%" -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" -DMUJOCO_BUILD_EXAMPLES=OFF -DMUJOCO_BUILD_TESTS=OFF -DMUJOCO_BUILD_SIMULATE=ON -DMUJOCO_TEST_PYTHON_UTIL=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-%MINGW32_CMAKE_EXE% --build "%BUILD_DIR%"
-%MINGW32_CMAKE_EXE% --install "%BUILD_DIR%"
+%CMAKE_EXECUTABLE% -S "%MUJOCO_SRC_DIR%" -B "%BUILD_DIR%" -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" -DMUJOCO_BUILD_EXAMPLES=OFF -DMUJOCO_BUILD_TESTS=OFF -DMUJOCO_BUILD_SIMULATE=ON -DMUJOCO_TEST_PYTHON_UTIL=OFF -DCMAKE_POLICY_VERSION_MINIMUM="3.5" -Wno-deprecated -Wno-dev
+%CMAKE_EXECUTABLE% --build "%BUILD_DIR%" --config Release
+%CMAKE_EXECUTABLE% --install "%BUILD_DIR%"
 
-copy /Y "%BUILD_DIR%\lib\multiverse_connector.dll" "%CD%\mujoco_plugin"
-xcopy /E /I /Y "%CD%\mujoco_plugin" "%INSTALL_DIR%\bin"
+copy /Y "%BUILD_DIR%\bin\Release\multiverse_connector.dll" "%CD%\mujoco_plugin"
+xcopy /E /I /Y "%CD%\mujoco_plugin" "%INSTALL_DIR%\bin\mujoco_plugin"
 
 for /f %%a in ('powershell -Command "[int](Get-Date -UFormat %%s)"') do set END_TIME=%%a
 set /a ELAPSED=%END_TIME% - %START_TIME%
--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ start_time=$(date +%s)
 CURRENT_DIR=$PWD
 cd $(dirname $0)
 SRC_DIR=$PWD/src
-
+MUJOCO_VERSION=3.3.0
+MUJOCO_SRC_DIR=$SRC_DIR/mujoco-$MUJOCO_VERSION
 if [ ! -d $SRC_DIR ]; then
     mkdir -p $SRC_DIR
-    MUJOCO_VERSION=3.3.0
     MUJOCO_TAR_FILE=mujoco-$MUJOCO_VERSION.tar.gz
     curl -L -o $SRC_DIR/$MUJOCO_TAR_FILE https://github.com/google-deepmind/mujoco/archive/refs/tags/$MUJOCO_VERSION.tar.gz
-    tar xf $SRC_DIR/$MUJOCO_TAR_FILE -C $SRC_DIR --strip-components=1
+    tar xf $SRC_DIR/$MUJOCO_TAR_FILE -C $MUJOCO_SRC_DIR --strip-components=1
     rm -f $SRC_DIR/$MUJOCO_TAR_FILE
 fi
 
@@ -20,18 +20,18 @@ INSTALL_DIR=$PWD/install
 if [ ! -d $INSTALL_DIR ]; then
     mkdir -p $INSTALL_DIR
 fi
-if [ ! -d $SRC_DIR/build ]; then
-    mkdir -p $SRC_DIR/build
+if [ ! -d $BUILD_DIR ]; then
+    mkdir -p $BUILD_DIR
 fi
 
-ln -sf $PWD/plugin/multiverse_connector $SRC_DIR/plugin
-CMAKE_PATH=$SRC_DIR/CMakeLists.txt
+ln -sf $PWD/plugin/multiverse_connector $MUJOCO_SRC_DIR/plugin
+CMAKE_PATH=$MUJOCO_SRC_DIR/CMakeLists.txt
 LINE_TO_ADD="add_subdirectory(plugin/multiverse_connector)"
 if ! grep -Fxq "$LINE_TO_ADD" "$CMAKE_PATH"; then
     echo "$LINE_TO_ADD" >> $CMAKE_PATH
 fi
 
-cmake -S $SRC_DIR -B $BUILD_DIR -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DMUJOCO_BUILD_EXAMPLES=OFF -DMUJOCO_BUILD_TESTS=OFF -DMUJOCO_BUILD_SIMULATE=ON -DMUJOCO_TEST_PYTHON_UTIL=OFF
+cmake -S $MUJOCO_SRC_DIR -B $BUILD_DIR -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DMUJOCO_BUILD_EXAMPLES=OFF -DMUJOCO_BUILD_TESTS=OFF -DMUJOCO_BUILD_SIMULATE=ON -DMUJOCO_TEST_PYTHON_UTIL=OFF
 cmake --build $BUILD_DIR
 cmake --install $BUILD_DIR
 
